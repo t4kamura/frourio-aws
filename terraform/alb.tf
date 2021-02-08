f1efcd4e85f2db75f848993207621cccae10ab23
takamuraTK <tirpitz.zzz@outlook.jp>
2021-02-09 06:14:28 +0900

    Add domain setting

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -53,6 +53,6 @@ resource "aws_lb_target_group" "main" {
 }
 
 resource "aws_acm_certificate" "frourio" {
-  domain_name       = "frourio.tkmrtk.com"
+  domain_name       = "frourio.example.com"
   validation_method = "EMAIL"
 }
